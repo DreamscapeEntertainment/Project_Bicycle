93e0e7b83ac2ad37d3bcdb43954cd36d71886b37
Erik Cerini <ejcerini@wpi.edu>
2018-03-17 17:20:14 -0400

    Base Card Creator with DB done!
    Completed: Card Creator with DB
    Next: Effects Conceptualization

--- Project_Bicycle/Assets/Scripts/CardCreator/CardCreator.cs
+++ Project_Bicycle/Assets/Scripts/CardCreator/CardCreator.cs
@@ -3,8 +3,23 @@ using UnityEngine;
 
 public class CardCreator : EditorWindow {
 
-    Card c;
+    enum STATE {
+        BLANK = 0,
+        ADD,
+        EDIT
+    }
+
+    Card newCard;
+    CardDatabase cards;
+    Vector2 _scrollPos;
+
+    int selectedCard;
+
+    STATE currentState;
+
     GUILayoutOption[] noOptions = new GUILayoutOption[0];
+
+    private const string DATABASE_PATH = @"Assets/Cards/cardDB.asset";
 
     [MenuItem("Dreamscape/Cards/Card Creator")]
     public static void Init() {
@@ -14,21 +29,132 @@ public class CardCreator : EditorWindow {
     }
 
     private void OnEnable() {
-        if(c == null)
-            c = Card.CreateInstance<Card>();
+        if(cards == null)
+            LoadDatabase();
+
+        currentState = STATE.BLANK;
     }
 
     private void OnGUI() {
         EditorGUILayout.BeginHorizontal(GUILayout.ExpandWidth(true));
 
-        DisplayCreator();
+        DisplayDatabase();
+        DisplayMain();
 
         EditorGUILayout.EndHorizontal();
     }
 
-    void DisplayCreator() {
+    void LoadDatabase() {
+        cards = (CardDatabase)AssetDatabase.LoadAssetAtPath(DATABASE_PATH, typeof(CardDatabase));
 
-        EditorGUILayout.BeginVertical(GUILayout.ExpandHeight(true));
+        if (cards == null)
+            CreateDatabase();
+    }
+
+    void CreateDatabase() {
+        cards = ScriptableObject.CreateInstance<CardDatabase>();
+        AssetDatabase.CreateAsset(cards, DATABASE_PATH);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+
+    void DisplayMain() {
+
+        EditorGUILayout.BeginVertical(GUILayout.ExpandWidth(true));
+        EditorGUILayout.Space();
+
+        switch (currentState) {
+            case STATE.ADD:
+                DisplayNewMode();
+                break;
+            case STATE.EDIT:
+                DisplayEditMode();
+                break;
+            default:
+                DisplayBlankArea();
+                break;
+        }
+
+        EditorGUILayout.Space();
+        EditorGUILayout.EndVertical();
+
+    }
+
+    void DisplayDatabase() {
+        EditorGUILayout.BeginVertical(GUILayout.MaxWidth(250));
+        EditorGUILayout.Space();
+
+        _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, "box", GUILayout.ExpandHeight(true));
+        
+        for(int cnt = 0; cnt < cards.COUNT; cnt++) {
+            EditorGUILayout.BeginHorizontal();
+            if(GUILayout.Button("-", GUILayout.Width(25))) {
+                cards.RemoveAt(cnt);
+                cards.SortAlphabeticallyAtoZ();
+                EditorUtility.SetDirty(cards);
+
+                currentState = STATE.BLANK;
+                return;
+            }
+
+            if (GUILayout.Button(cards.Card(cnt).cardName, "box", GUILayout.ExpandWidth(true))){
+                selectedCard = cnt;
+
+                currentState = STATE.EDIT;
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        EditorGUILayout.EndScrollView();
+
+        EditorGUILayout.BeginHorizontal(GUILayout.ExpandWidth(true));
+        EditorGUILayout.LabelField("Cards: " + cards.COUNT, GUILayout.Width(100));
+
+        if (GUILayout.Button("New Card")) {
+            newCard = ScriptableObject.CreateInstance<Card>();
+            currentState = STATE.ADD;
+        }
+
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.Space();
+        EditorGUILayout.EndVertical();
+    }
+
+    public void DisplayBlankArea() {
+
+        GUI.skin.label.wordWrap = true;
+
+        EditorGUILayout.LabelField(
+          "Select a card in the database to the right \n" +
+          "or create a new one!",
+          GUILayout.ExpandHeight(true));
+    }
+
+    public void DisplayEditMode() {
+
+        cards.SetAtIndex(selectedCard, DisplayCardCreator(cards.Card(selectedCard)));
+
+        if (GUILayout.Button("Done", GUILayout.Width(100))) {
+            cards.SortAlphabeticallyAtoZ();
+            EditorUtility.SetDirty(cards);
+            currentState = STATE.BLANK;
+        }
+    }
+
+    public void DisplayNewMode() {
+        newCard = DisplayCardCreator(newCard);
+
+        if(GUILayout.Button("Done", GUILayout.Width(100))) {
+            cards.Add(newCard);
+            cards.SortAlphabeticallyAtoZ();
+
+            EditorUtility.SetDirty(cards);
+            currentState = STATE.BLANK;
+        }
+    }
+
+    public Card DisplayCardCreator(Card card) {
+        Card c = card;
 
         EditorGUILayout.LabelField("Card Data", EditorStyles.boldLabel);
         c.cardName = EditorGUILayout.TextField("Card Name", c.cardName, GUILayout.MaxWidth(400));
@@ -38,10 +164,8 @@ public class CardCreator : EditorWindow {
 
         c.isInvoke = EditorGUILayout.Toggle("Invoke?", c.isInvoke);
         c.isEcho = EditorGUILayout.Toggle("Echo?", c.isEcho);
-        EditorGUILayout.Space();
 
-        EditorGUILayout.EndVertical();
-
+        return c;
     }
 
 }
---
+++ Project_Bicycle/Assets/Scripts/CardCreator/CardDatabase.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CardDatabase : ScriptableObject {
+    [SerializeField]
+    private List<Card> database;
+
+    void OnEnable() {
+        if (database == null)
+            database = new List<Card>();
+    }
+
+    public void Add(Card card) {
+        database.Add(card);
+    }
+
+    public void Remove(Card card) {
+        database.Remove(card);
+    }
+
+    public void RemoveAt(int index) {
+        database.RemoveAt(index);
+    }
+
+    public int COUNT {
+        get { return database.Count; }
+    }
+
+    //.ElementAt() requires the System.Linq
+    public Card Card(int index) {
+        return database.ElementAt(index);
+    }
+
+    public void SortAlphabeticallyAtoZ() {
+        database.Sort((x, y) => string.Compare(x.cardName, y.cardName));
+    }
+
+    public void SetAtIndex(int id, Card c) {
+        database[id] = c;
+    }
+}
